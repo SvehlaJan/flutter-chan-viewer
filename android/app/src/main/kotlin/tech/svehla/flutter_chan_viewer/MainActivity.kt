79f00f19476d1dee5ed46e6f312680fb1ae64ba4
Ján Švehla <jan.svehla@avast.com>
2020-07-16 08:56:54 +0200

    Update Android activity & splash screen

--- android/app/src/main/kotlin/tech/svehla/flutter_chan_viewer/MainActivity.kt
+++ android/app/src/main/kotlin/tech/svehla/flutter_chan_viewer/MainActivity.kt
@@ -1,12 +1,12 @@
 package tech.svehla.flutter_chan_viewer
 
 import androidx.annotation.NonNull;
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
-import android.view.WindowManager.LayoutParams;
+import android.view.WindowManager.LayoutParams
 
-class MainActivity : FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
 
